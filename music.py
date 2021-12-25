99e76da5f7027d4516c35a450109a6cb02981931
RCSnake <73113123+RCSnake@users.noreply.github.com>
2021-12-25 22:16:32 +0000

    Update music.py

--- music.py
+++ music.py
@@ -1,12 +1,58 @@
 import discord
-import youtube_dl
+import asyncio
 from discord.ext import commands 
+import youtube_dl
 
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': 'downloads/%(id)s.mp3',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=1):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.id = data.get('id')
+        self.title = data.get('title')
+        self.url = data.get('url')
+    
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+            #print(data)
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 class music(commands.Cog):
-    def __init__(self, client):
-        self.client = client 
+    def __init__(self,bot):
+        self.bot = bot
+        self.queue = []#Queue() # Unlimited size queue
 
+        self.REMOVE_DOWNLOADS = True
+    
     @commands.command()
     async def join(self,ctx):
       if ctx.author.voice is None:
@@ -21,18 +67,29 @@ class music(commands.Cog):
     async def disconnect(self,ctx):
       await ctx.voice_client.disconnect()
 
-    @commands.command()
-    async def play(self,ctx,url):
-      ctx.voice_client.stop()
-      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options':  '-vn'}
-      YDL_OPTIONS = {'format': "bestaudio"}
-      vc = ctx.voice_client
+    @commands.command(name='play', description='Play\'s music', aliases=['p'])
+    async def play(self, ctx, *, url):
+        if ctx.voice_client == None:
+            await ctx.author.voice.channel.connect()
 
-      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(url, download=False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        vc.play(source)
+        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+        #self.queue.insert(0,player)
+
+        if ctx.voice_client.is_playing():
+            self.queue.insert(0,player)
+            await ctx.send(f'Queued: `{url}`')
+        else:
+            def loop_play(e):
+                if e:
+                    print('Player error: %s' % e)
+                else:
+                    if len(self.queue) > 0:
+                        ctx.voice_client.play(self.queue.pop(), after=loop_play)
+                    else:
+                        return
+
+            ctx.voice_client.play(player, after=loop_play)
+        await self.nowplaying(ctx)
 
     @commands.command()
     async def pause(self,ctx):
@@ -44,7 +101,16 @@ class music(commands.Cog):
       await ctx.voice_client.resume()
       await ctx.send("resume")
 
-
+    @commands.command(name='skip', description='Stops current playing music and plays next in the queue',aliases=['s','n','next'])
+    async def skip(self,ctx, next_idx=1):
+        if next_idx < 0 or next_idx % int(next_idx) or next_idx > len(self.queue):
+            print('invalid arg')
+            return
+        if ctx.voice_client.is_playing():
+            if next_idx != 1:
+                self.queue = self.queue[:(-1)*(next_idx-1)]
+            ctx.voice_client.stop()
+            print('ctx.voice_client.stop()')
 
 def setup(client):
     client.add_cog(music(client))
