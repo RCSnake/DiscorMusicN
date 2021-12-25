7206a0a6eedce031e6b2d8ff3dd5ca8dd249640c
RCSnake <73113123+RCSnake@users.noreply.github.com>
2021-12-25 22:32:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ client = commands.Bot(command_prefix='-', intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-
+@client.event
+async def on_message_edit(before, after):
+    await client.process_commands(after)
 
 client.run("Token")
